25f643ec1fb3cbb7392a3436f8b93e1d7c786881
metabrez <metabrez@gmail.com>
2018-11-30 11:10:18 -0500

    password validator using pristine properties

--- src/app/shared/password.validator.ts
+++ src/app/shared/password.validator.ts
@@ -4,6 +4,10 @@ import { AbstractControl } from "@angular/forms";
 export function PasswordValidator(control:AbstractControl):{[key:string]:boolean} | null{
 const password=control.get('password');
 const confirmPassword=control.get('confirmPassword');
+//When password and confirm password is given and mismatch then it shows error
+if(password.pristine || confirmPassword.pristine){
+    return null;
+}
 return password && confirmPassword && password.value!==confirmPassword.value?{'misMatch':true}:
 null;
 
