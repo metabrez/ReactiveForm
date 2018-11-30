333c25d862b8b02ecc4911bc9e797d3519947a54
metabrez <metabrez@gmail.com>
2018-11-30 14:01:53 -0500

    Conditional Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder,Validator, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validator, Validators, FormGroup} from '@angular/forms';
 import { forbiddenNameValidator } from './shared/user-name.validator';
 import { PasswordValidator } from './shared/password.validator';
 
@@ -8,29 +8,56 @@ import { PasswordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  //This get userName() is used in app.component.html for making code cleaner
+  registrationForm:FormGroup;
+ 
   
+  title = 'reactive-form';
+  constructor(private fb:FormBuilder){}
   get userName(){
     return this.registrationForm.get('userName');
   }
-  title = 'reactive-form';
-  constructor(private fb:FormBuilder){}
-
-  registrationForm = this.fb.group({
-    userName:['',[Validators.required,Validators.minLength(3),forbiddenNameValidator(/password/)]],
-    password:[''],
-    confirmPassword:[''],
-    address:this.fb.group({
-      city:[''],
-      state:[''],
-      postalCode:['']
-    })
+  get email(){
+    return this.registrationForm.get('email');
   }
-  ,
-  {validator:PasswordValidator}
-  );
+ 
+
+  //This get userName() is used in app.component.html for making code cleaner
+  ngOnInit(){
+    this.registrationForm = this.fb.group({
+      userName:['',[Validators.required,Validators.minLength(3),forbiddenNameValidator(/password/)]],
+      email:[''],
+      subscribe:[false],
+      password:[''],
+      confirmPassword:[''],
+      address:this.fb.group({
+        city:[''],
+        state:[''],
+        postalCode:['']
+      })
+    }
+    ,
+    {validator:PasswordValidator}
+    );
+  
+    this.registrationForm.get('subscribe').valueChanges
+    .subscribe(
+      checkedValue =>{
+        const email = this.registrationForm.get('email');
+        if(checkedValue){
+          email.setValidators(Validators.required);
+        }
+        else{
+          email.clearValidators();
+        }
+        email.updateValueAndValidity();
+      }
+    );
+
+  }
+  
+ 
 
  /*  registrationForm=new FormGroup(
     {userName:new FormControl('Tabrez'),
