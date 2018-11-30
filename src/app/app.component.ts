32b9e7dd731159127a08baf4f892e954093f6f57
metabrez <metabrez@gmail.com>
2018-11-29 20:07:16 -0500

    commmunicating with model with view html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-form';
+  registrationForm=new FormGroup(
+    {userName:new FormControl('Tabrez'),
+  password:new FormControl(''),
+  confirmPassword:new FormControl('')
+  });
 }
